6f6440c6babbb01c2e36f4d81a42d0309e460d53
Jason Day <jason.day.blue@gmail.com>
2022-02-25 02:40:33 -0600

    updated contact page with blocks

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,34 +2,78 @@ import React from "react";
 
 export default function Contact() {
 	return (
-		<div class="m-4">
-			<h1>Contact</h1>
-			<div class="">
-				<label
-					for="exampleFormControlInput1"
-					class="form-label"
-				>
-					Email address
-				</label>
-				<input
-					type="email"
-					class="form-control"
-					id="exampleFormControlInput1"
-					placeholder="name@example.com"
-				/>
+		<div class="container">
+			<div class="block">
+				<article class="message is-danger">
+					<div class="message-header">
+						<p>Error</p>
+						<button
+							class="delete"
+							aria-label="delete"
+						></button>
+					</div>
+					<div class="message-body">
+						Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.{" "}
+						<strong>
+							Pellentesque risus mi
+						</strong>
+						, tempus quis placerat ut, porta
+						nec nulla. Vestibulum rhoncus ac
+						ex sit amet fringilla. Nullam
+						gravida purus diam, et dictum{" "}
+						<a>felis venenatis</a>{" "}
+						efficitur.
+					</div>
+				</article>
 			</div>
-			<div class="mb-3">
-				<label
-					for="exampleFormControlTextarea1"
-					class="form-label"
-				>
-					Message:
-				</label>
-				<textarea
-					class="form-control"
-					id="exampleFormControlTextarea1"
-					rows="3"
-				></textarea>
+			<div class="block">
+				<article class="message is-danger">
+					<div class="message-header">
+						<p>Error</p>
+						<button
+							class="delete"
+							aria-label="delete"
+						></button>
+					</div>
+					<div class="message-body">
+						Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.{" "}
+						<strong>
+							Pellentesque risus mi
+						</strong>
+						, tempus quis placerat ut, porta
+						nec nulla. Vestibulum rhoncus ac
+						ex sit amet fringilla. Nullam
+						gravida purus diam, et dictum{" "}
+						<a>felis venenatis</a>{" "}
+						efficitur.
+					</div>
+				</article>
+			</div>
+			<div class="block">
+				<article class="message is-danger">
+					<div class="message-header">
+						<p>Error</p>
+						<button
+							class="delete"
+							aria-label="delete"
+						></button>
+					</div>
+					<div class="message-body">
+						Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.{" "}
+						<strong>
+							Pellentesque risus mi
+						</strong>
+						, tempus quis placerat ut, porta
+						nec nulla. Vestibulum rhoncus ac
+						ex sit amet fringilla. Nullam
+						gravida purus diam, et dictum{" "}
+						<a>felis venenatis</a>{" "}
+						efficitur.
+					</div>
+				</article>
 			</div>
 		</div>
 	);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,52 +5,509 @@ import React from "react";
 
 export default function Home() {
 	return (
-		<div id="background" class="m-4">
-			<h1>Projects</h1>
-			<div class="container p-5" id="projects">
-				<figure class="figure">
-					<a href="https://github.com/shivermotion/ScribbleTake">
-						{/* <img
-							src={photo1}
-							class="figure-img img-fluid rounded"
-							id="project"
-							alt="date date-night-creator-snapshotv2"
-						/> */}
-					</a>
-					<figcaption class="figure-caption">
-						NoteTaker
-					</figcaption>
-				</figure>
+		<div class="container">
+			<section class="articles">
+				<div class="column is-8 is-offset-2">
+					<div class="card article">
+						<div class="card-content">
+							<div class="media">
+								<div class="media-content has-text-centered">
+									<p class="title article-title">
+										Introducing
+										a
+										new
+										feature
+										for
+										paid
+										subscribers
+									</p>
+									<div class="tags has-addons level-item">
+										<span class="tag is-rounded is-info">
+											@skeetskeet
+										</span>
+										<span class="tag is-rounded">
+											May
+											10,
+											202X
+										</span>
+									</div>
+								</div>
+							</div>
+							<div class="content article-body">
+								<p>
+									Non arcu
+									risus
+									quis
+									varius
+									quam
+									quisque.
+									Dictum
+									varius
+									duis at
+									consectetur
+									lorem.
+									Posuere
+									sollicitudin
+									aliquam
+									ultrices
+									sagittis
+									orci a
+									scelerisque
+									purus
+									semper.{" "}
+								</p>
+								<p>
+									Metus
+									aliquam
+									eleifend
+									mi in
+									nulla
+									posuere
+									sollicitudin
+									aliquam
+									ultrices.
+									In hac
+									habitasse
+									platea
+									dictumst
+									vestibulum
+									rhoncus
+									est
+									pellentesque
+									elit.
+									Accumsan
+									lacus
+									vel
+									facilisis
+									volutpat.
+									Non
+									sodales
+									neque
+									sodales
+									ut
+									etiam.
+									Est
+									pellentesque
+									elit
+									ullamcorper
+									dignissim
+									cras
+									tincidunt
+									lobortis
+									feugiat
+									vivamus.
+								</p>
+								<h3 class="has-text-centered">
+									How to
+									properly
+									center
+									tags in
+									bulma?
+								</h3>
+								<p>
+									{" "}
+									Proper
+									centering
+									of tags
+									in bulma
+									is done
+									with
+									class:{" "}
+									<pre>
+										level-item
+									</pre>
+									Voluptat
+									ut
+									farmacium
+									tellus
+									in metus
+									vulputate.
+									Feugiat
+									in
+									fermentum
+									posuere
+									urna
+									nec.
+									Pharetra
+									convallis
+									posuere
+									morbi
+									leo urna
+									molestie.
+									Accumsan
+									lacus
+									vel
+									facilisis
+									volutpat
+									est
+									velit
+									egestas.
+									Fermentum
+									leo vel
+									orci
+									porta.
+									Faucibus
+									interdum
+									posuere
+									lorem
+									ipsum.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="card article">
+						<div class="card-content">
+							<div class="media">
+								<div class="media-center">
+									<img
+										src="https://res.cloudinary.com/ameo/image/upload/v1639144778/typocat_svbspx.png"
+										class="author-image"
+										alt="Placeholder image"
+									/>
+								</div>
+								<div class="media-content has-text-centered">
+									<p class="title article-title">
+										Sapien
+										eget
+										mi
+										proin
+										sed
+										🔱
+									</p>
+									<p class="subtitle is-6 article-subtitle">
+										<a href="#">
+											@daria
+										</a>{" "}
+										on
+										February
+										17,
+										202X
+									</p>
+								</div>
+							</div>
+							<div class="content article-body">
+								<p>
+									Lorem
+									ipsum
+									dolor
+									sit
+									amet,
+									consectetur
+									adipiscing
+									elit,
+									sed do
+									eiusmod
+									tempor
+									incididunt
+									ut
+									labore
+									et
+									dolore
+									magna
+									aliqua.
+									Accumsan
+									lacus
+									vel
+									facilisis
+									volutpat
+									est
+									velit
+									egestas.
+									Sapien
+									eget mi
+									proin
+									sed. Sit
+									amet
+									mattis
+									vulputate
+									enim.
+								</p>
+								<p>
+									Commodo
+									ullamcorper
+									a lacus
+									vestibulum
+									sed
+									arcu.
+									Fermentum
+									leo vel
+									orci
+									porta
+									non.
+									Proin
+									fermentum
+									leo vel
+									orci
+									porta
+									non
+									pulvinar.
+									Imperdiet
+									proin
+									fermentum
+									leo vel.
+									Tortor
+									posuere
+									ac ut
+									consequat
+									semper
+									viverra.
+									Vestibulum
+									lectus
+									mauris
+									ultrices
+									eros.
+								</p>
+								<h3 class="has-text-centered">
+									Lectus
+									vestibulum
+									mattis
+									ullamcorper
+									velit
+									sed
+									ullamcorper
+									morbi.
+									Cras
+									tincidunt
+									lobortis
+									feugiat
+									vivamus.
+								</h3>
+								<p>
+									In eu mi
+									bibendum
+									neque
+									egestas
+									congue
+									quisque
+									egestas
+									diam.
+									Enim nec
+									dui nunc
+									mattis
+									enim ut
+									tellus.
+									Ut morbi
+									tincidunt
+									augue
+									interdum
+									velit
+									euismod
+									in. At
+									in
+									tellus
+									integer
+									feugiat
+									scelerisque
+									varius
+									morbi
+									enim
+									nunc.
+									Vitae
+									suscipit
+									tellus
+									mauris a
+									diam.
+									Arcu non
+									sodales
+									neque
+									sodales
+									ut etiam
+									sit
+									amet.
+								</p>
+							</div>
+						</div>
+					</div>
 
-				<figure class="figure">
-					<a href="https://shivermotion.github.io/work_day_scheduler/">
-						{/* <img
-							src={photo3}
-							class="figure-img img-fluid rounded"
-							id="project"
-							alt="work day scheduler"
-						/> */}
-					</a>
-					<figcaption class="figure-caption">
-						Work Day Scheduler
-					</figcaption>
-				</figure>
-
-				<figure class="figure">
-					<a href="https://young-anchorage-47013.herokuapp.com/">
-						{/* <img
-							src={photo2}
-							class="figure-img img-fluid rounded"
-							id="project"
-							alt="LookBook"
-						/> */}
-					</a>
-					<figcaption class="figure-caption">
-						Look Book - a social app for
-						readers
-					</figcaption>
-				</figure>
-			</div>
+					<section class="hero is-info is-bold is-small promo-block">
+						<div class="hero-body">
+							<div class="container">
+								<h1 class="title">
+									<i class="fa fa-bell-o"></i>{" "}
+									Nemo
+									enim
+									ipsam
+									voluptatem
+									quia.
+								</h1>
+								<span class="tag is-black is-medium is-rounded">
+									Natus
+									error
+									sit
+									voluptatem
+								</span>
+							</div>
+						</div>
+					</section>
+					<div class="card article">
+						<div class="card-content">
+							<div class="media">
+								<div class="media-center">
+									<img
+										src="https://res.cloudinary.com/ameo/image/upload/v1639144778/typocat_svbspx.png"
+										class="author-image"
+										alt="Placeholder image"
+									/>
+								</div>
+								<div class="media-content has-text-centered">
+									<p class="title article-title">
+										Cras
+										tincidunt
+										lobortis
+										feugiat
+										vivamus.
+									</p>
+									<p class="subtitle is-6 article-subtitle">
+										<a href="#">
+											@angela
+										</a>{" "}
+										on
+										October
+										7,
+										202X
+									</p>
+								</div>
+							</div>
+							<div class="content article-body">
+								<p>
+									Lorem
+									ipsum
+									dolor
+									sit
+									amet,
+									consectetur
+									adipiscing
+									elit,
+									sed do
+									eiusmod
+									tempor
+									incididunt
+									ut
+									labore
+									et
+									dolore
+									magna
+									aliqua.
+									Accumsan
+									lacus
+									vel
+									facilisis
+									volutpat
+									est
+									velit
+									egestas.
+									Sapien
+									eget mi
+									proin
+									sed. Sit
+									amet
+									mattis
+									vulputate
+									enim.
+								</p>
+								<p>
+									Commodo
+									ullamcorper
+									a lacus
+									vestibulum
+									sed
+									arcu.
+									Fermentum
+									leo vel
+									orci
+									porta
+									non.
+									Proin
+									fermentum
+									leo vel
+									orci
+									porta
+									non
+									pulvinar.
+									Imperdiet
+									proin
+									fermentum
+									leo vel.
+									Tortor
+									posuere
+									ac ut
+									consequat
+									semper
+									viverra.
+									Vestibulum
+									lectus
+									mauris
+									ultrices
+									eros.
+								</p>
+								<h3 class="has-text-centered">
+									“Everyone
+									should
+									be able
+									to do
+									one card
+									trick,
+									tell two
+									jokes,
+									and
+									recite
+									three
+									poems,
+									in case
+									they are
+									ever
+									trapped
+									in an
+									elevator.”
+								</h3>
+								<p>
+									In eu mi
+									bibendum
+									neque
+									egestas
+									congue
+									quisque
+									egestas
+									diam.
+									Enim nec
+									dui nunc
+									mattis
+									enim ut
+									tellus.
+									Ut morbi
+									tincidunt
+									augue
+									interdum
+									velit
+									euismod
+									in. At
+									in
+									tellus
+									integer
+									feugiat
+									scelerisque
+									varius
+									morbi
+									enim
+									nunc.
+									Vitae
+									suscipit
+									tellus
+									mauris a
+									diam.
+									Arcu non
+									sodales
+									neque
+									sodales
+									ut etiam
+									sit
+									amet.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
 		</div>
 	);
 }
